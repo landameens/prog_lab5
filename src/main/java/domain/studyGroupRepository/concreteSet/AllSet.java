bf84e0db4ad64659c7544c8d6c37415a3b78bd10
Yaroslav <yaroslavsoldat2@gmail.com>
2020-03-13 00:03:14 +0300

    Code review

--- src/main/java/domain/studyGroupRepository/concreteSet/AllSet.java
+++ src/main/java/domain/studyGroupRepository/concreteSet/AllSet.java
@@ -6,6 +6,7 @@ import domain.studyGroup.StudyGroup;
 import java.util.TreeSet;
 
 public final class AllSet extends ConcreteSet {
+    //TODO: поменять сигнатуру метода (была изменена в абстрактном классе)
     @Override
     public TreeSet<StudyGroup> execute(TreeSet<StudyGroup> studyGroups) throws StudyGroupRepositoryException {
         TreeSet<StudyGroup> finalStudyGroup = new TreeSet<>();
--- src/main/java/domain/studyGroupRepository/concreteSet/MinSet.java
+++ src/main/java/domain/studyGroupRepository/concreteSet/MinSet.java
@@ -8,6 +8,9 @@ import java.util.TreeSet;
 
 public class MinSet extends ConcreteSet {
 
+    //TODO: поменять сигнатуру метода (была изменена в абстрактном классе)
+    //TODO: пометить как final класс
+    //TODO: расширяй дальше Concrete set САМА, ЕСЛИ ЕСТЬ ВОПРОСЫ СПРАШИВАЙ, А НЕ КОПИПАСТЬ
     @Override
     public TreeSet<StudyGroup> execute(TreeSet<StudyGroup> studyGroups) throws StudyGroupRepositoryException {
         Comparator<StudyGroup> studyGroupComparator = new StudyGroup.StudyGroupComparator();
