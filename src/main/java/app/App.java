56756de02bd3a7d1592c33c99e252b1558d0949d
Nadya <kirillova202001@yandex.ru>
2020-03-19 22:44:55 +0300

    get path

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -17,12 +17,45 @@ import domain.studyGroupRepository.TreeSetStudyGroupRepository;
 import storage.StudyGroupDAO;
 import storage.exception.DAOException;
 
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.Set;
 
 public class App {
+    private static void checkInputPath(String[] args) {
+        if (args.length != 2) {
+            System.err.println(LACK_OF_ARGUMENTS_ERROR);
+            System.exit(1);
+        }
+    }
+    private static final String LACK_OF_ARGUMENTS_ERROR = "Неверный путь. Введите в формате {absolute/relative} {path to the fail}";
+
     public static void main(String[] args) throws StudyGroupRepositoryException, VerifyException, DAOException {
-        String path = "C:\\Users\\user\\Desktop\\Programming\\prog_lab5\\src\\main\\resources\\studyGroups";
+        ClassLoader classLoader = App.class.getClassLoader();
+        String path = "";
+
+
+        if (args.length > 0) {
+            checkInputPath(args);
+
+            PathModifiers modifier = PathModifiers.getPathModifiers(args[1]);
+
+            if (modifier.equals(PathModifiers.ABSOLUTE)) {
+                path = args[2];
+            }
+
+            if (modifier.equals(PathModifiers.RELATIVE)) {
+                URL fileURL = classLoader.getResource(args[2]);
+                path = fileURL.getFile();
+            }
+
+            if (modifier == null) {
+                System.err.println(LACK_OF_ARGUMENTS_ERROR);
+            }
+
+        }
+
+
         IdProducer idProducer = new IdProducer();
         StudyGroupFactory studyGroupFactory = new StudyGroupFactory(idProducer);
         TreeSetStudyGroupRepository studyGroupRepository = new TreeSetStudyGroupRepository(studyGroupFactory, path);
@@ -50,4 +83,5 @@ public class App {
         //studyGroupRepository.returnStudyGroup();
         //studyGroupRepository.save();
     }
+
 }
